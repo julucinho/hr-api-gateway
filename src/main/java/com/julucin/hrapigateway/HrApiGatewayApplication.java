7ba3f3b290a5620b9b48621871002bce8c2c4b08
Zé Lúcio <joselucioalmeidajunior@gmail.com>
2021-12-22 06:05:13 -0300

    feat: starting project

---
+++ src/main/java/com/julucin/hrapigateway/HrApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.julucin.hrapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class HrApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayApplication.class, args);
+	}
+
+}
